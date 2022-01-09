cec0167f269fc4823e4b7f20f0c6191b6b7da35b
Kelerik <vincent.ng39@gmail.com>
2022-01-09 12:20:09 -0500

    display roles and display employees table

--- index.js
+++ index.js
@@ -46,8 +46,10 @@ const promptMenu = () => {
                viewDepartments();
                break;
             case "View all roles":
+               viewRoles();
                break;
             case "View all employees":
+               viewEmployees();
                break;
             case "Add a department":
                break;
@@ -69,11 +71,57 @@ const promptMenu = () => {
 // display departments
 const viewDepartments = () => {
    const sql = `
-SELECT
-   department.id AS ID,
-   department.name AS "Department Name"
-FROM
-   department`;
+   SELECT
+      department.id AS ID,
+      department.name AS "Department Name"
+   FROM
+      department`;
+   db.query(sql, (err, rows) => {
+      if (err) {
+         console.log({ error: err.message });
+      } else {
+         updateBottomBar(rows);
+      }
+   });
+};
+
+// display roles
+const viewRoles = () => {
+   const sql = `
+   SELECT
+      role.id AS ID,
+      role.title AS "Role Title",
+      role.salary AS Salary,
+      department.name AS Department
+   FROM
+      role
+      LEFT JOIN department ON role.department_id = department.id`;
+   db.query(sql, (err, rows) => {
+      if (err) {
+         console.log({ error: err.message });
+      } else {
+         updateBottomBar(rows);
+      }
+   });
+};
+
+// display employees
+const viewEmployees = () => {
+   const sql = `
+   SELECT
+      a.id AS ID,
+      a.first_name AS "First Name",
+      a.last_name AS "Last Name",
+      role.title AS Role,
+      department.name AS Department,
+      role.salary AS Salary,
+      -- replace with string if null
+      coalesce(b.first_name, '-') AS Manager
+   FROM
+      employee a
+      LEFT JOIN employee b ON a.manager_id = b.id
+      LEFT JOIN role ON a.role_id = role.id
+      LEFT JOIN department ON role.department_id = department.id`;
    db.query(sql, (err, rows) => {
       if (err) {
          console.log({ error: err.message });
